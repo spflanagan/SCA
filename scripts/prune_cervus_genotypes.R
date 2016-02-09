8652228a163408cd37224aaf77bac95fef2da128
spflanagan <sflanagan19@gmail.com>
2016-02-09 15:41:25 -0600

    Feb9 work

--- scripts/prune_cervus_genotypes.R
+++ scripts/prune_cervus_genotypes.R
@@ -12,6 +12,51 @@ prune.loci<-function(df, prop){
 	return(new.df)
 }
 
+hwe.test<-function(df){#must be a df with two columns
+	locus.name<-substr(names(df)[1],1,nchar(names(df)[1])-1)
+	locus<-data.frame(A=df[df[,1]!="0",1],B=df[df[,2]!="0",2])
+	gt.sort<-apply(locus,1,sort)
+	locus$AB<-paste(gt.sort[1,],gt.sort[2,],sep="/")
+	af<-table(c(as.character(locus$A),as.character(locus$B)))/
+		sum(table(c(as.character(locus$A),as.character(locus$B))))
+	gf<-table(locus$AB)
+	result<-data.frame(genotype=character(),exp=numeric(),obs=numeric(),
+		stringsAsFactors=F)
+	for(i in 1:length(af)){ #calculate expected values
+		for(j in 1:length(af)){
+			names.sort<-sort(names(af)[c(i,j)])
+			pq.name<-paste(names.sort[1],names.sort[2],sep="/")
+			if(i == j){ 
+				pq.exp<-af[i]*af[j]
+			} else {	
+				pq.exp<-2*af[i]*af[j]
+			}
+			if(length(gf[names(gf) %in% pq.name])>0){
+				pq.obs<-gf[names(gf) %in% pq.name] 
+				result[nrow(result)+1,]<-
+					c(genotype=as.character(pq.name),exp=pq.exp,obs=pq.obs)
+			} else {
+				result[nrow(result)+1,]<-
+					c(genotype=as.character(pq.name),exp=pq.exp,obs=rep(0,1))
+			}
+		}#end j
+	}#end i
+	#result has some duplicates because the order of alleles is not guaranteed.
+	#test to see if expected = observed
+	result<-result[!duplicated(result$genotype),]
+	if(sum(as.numeric(result$exp)) != "1"){
+		print("expected frequencies do not sum to 1")
+		chi.result<-NA
+	}else{
+		result$exp<-round(as.numeric(result$exp)*sum(as.numeric(result$obs)))
+			result<-result[result$exp!="0",]
+		result$chi<-((as.numeric(result$obs)-as.numeric(result$exp))^2)/
+			as.numeric(result$exp)
+		chi.result<-1-pchisq(sum(result$chi),length(af)-1)
+	}
+	return(cbind(locus.name,chi.result))
+}#end hwe.test function
+
 setwd("E://ubuntushare//SCA//results//parentage")
 #multiple files
 genotypes.files<-list.files(pattern="genotypes[0-9]+.txt")
@@ -35,33 +80,21 @@ for(i in 1:length(genotypes.files)){
 
 #large file
 genotypes<-read.delim("genotypes.txt")
-gen90<-prune.loci(genotypes,0.1)	
-locus<-data.frame(A=gen90[gen90[,2]!="0",2],B=gen90[gen90[,3]!="0",3],
-	AB=paste(gen90[gen90[,2]!="0",2],gen90[gen90[,3]!="0",3],sep="/"))
-af<-table(c(as.character(locus$A),as.character(locus$B)))/
-	sum(table(c(as.character(locus$A),as.character(locus$B)))
-gf<-table(locus$AB)/sum(table(locus$AB))
-result<-data.frame(genotype=character(),exp=numeric(),obs=numeric(),
-	stringsAsFactors=F)
-for(i in 1:length(af)){ #calculate expected values
-	for(j in 1:length(af)){
-		pq.name<-paste(names(af)[i],names(af)[j],sep="/")
-		pq.exp<-2*af[i]*af[j]
-		if(length(gf[names(gf) %in% pq.name])>0){
-			pq.obs<-gf[names(gf) %in% pq.name] 
-			result[nrow(result)+1,]<-
-				c(genotype=as.character(pq.name),exp=pq.exp,obs=pq.obs)
-		} else {
-			result[nrow(result)+1,]<-
-				c(genotype=as.character(pq.name),exp=pq.exp,obs=rep(0,1))
-		}
-	}#end j
-}#end i
-#result has some duplicates because the order of alleles is not guaranteed.
-#test to see if expected = observed
 
-hwt<-chisq.test(result$obs,result$exp)
-	
+genotypes<-read.delim("genotypes99_10loci.txt")
+#prune to remove non-polymorphic loci
+consensus.num<-apply(genotypes,2,function(x){ length(which(x=="consensus")) })
+all.poly<-consensus.num[consensus.num==0]
+poly<-genotypes[,colnames(genotypes) %in% names(all.poly)]
+#remove any found in less than 90% of individuals
+gen90<-prune.loci(poly,0.1)
+#remove those loci not in hardy weinberg equilibrium
+hwe<-data.frame()
+for(x in seq(2,ncol(gen90),2)){
+	hwe<-rbind(hwe,hwe.test(gen90[,c(x,(x+1))]))
+}
+keep.loci<-hwe[as.numeric(as.character(hwe$chi.result))>0.05,]
+
 
 ###remove consensus
 consensus.num<-apply(all.pruned,2,function(x){ length(which(x=="consensus")) })
