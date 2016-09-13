c7fac52510dad1474f48889b13a4ed2b6faf0343
Sarah <spflanagan.phd@gmail.com>
2016-09-13 18:10:23 -0400

    13 Sept 2016

---
+++ programs/insilico_radseq/insilico_radseq.cpp
@@ -0,0 +1,172 @@
+//Author: Sarah P. Flanagan
+//Last Updated: 13 September 2016
+//Date Started: 13 September 2016
+//Purpose: Use restriction enzyme recognition sites to digest a genome file to compare sdRAD and ddRAD
+//also impose allelic dropout, PCR bias, shearing bias, etc.
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+void FileTest(ifstream& file, string filename)
+{
+	cout << filename;
+	if (file.is_open())
+		cout << " open\n";
+	else
+	{
+		while (!file.is_open())
+		{
+			cout << " not open. Please re-enter filename: ";
+			getline(cin, filename, '\n');
+			file.open(filename);
+		}
+	}
+
+}
+
+istream& universal_getline(istream& is, string& t)
+{
+	//this code is adapted from a post on stackoverflow:
+	// http://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+	//written by user763305
+	t.clear();
+	istream::sentry se(is, true);
+	streambuf* sb = is.rdbuf();//sets pointer to stream buffer object
+
+	for (;;)
+	{
+		int c = sb->sbumpc();//get current character and advance to the next position
+		switch (c)//tests for equality against a list of variables (like multiple if statements)
+		{
+		case '\n'://if the next character is '\n', return the line
+			return is;
+		case '\r'://if the character is '\n', see if the next one is '\n' or return the line
+			if (sb->sgetc() == '\n')
+				sb->sbumpc();
+			return is;
+		case EOF:// Also handle the case when the last line has no line ending
+			if (t.empty())//if there's nothing there, set it to be the end of file
+				is.setstate(ios::eofbit);//set it to be the end of the file and return it
+			return is;
+		default://if none of the above, continue on.
+			t += (char)c;
+		}
+	}
+
+}
+
+
+class fasta_record
+{
+public:
+	string seq_id;
+	string sequence;
+
+	fasta_record()
+	{
+		seq_id = "";
+		sequence = "";
+	}
+};
+
+class restriction_enzyme
+{
+public:
+	string rec_seq;
+	string overhang;
+
+	restriction_enzyme()
+	{
+		overhang = rec_seq = string();
+	}
+
+};
+
+
+int main()
+{
+	int i,ii,count, start, end;
+	string genome_name, digest_name, line, overhang;
+	restriction_enzyme enz1, enz2;
+	ifstream genome_file;
+	ofstream digest_file;
+	vector<fasta_record> genome;
+	vector<fasta_record> digest;
+
+	genome_name = "../../SSC_integrated.fa";
+	digest_name = "SSC_digested.fasta";
+	enz1.rec_seq = "CTGCAG"; //PstI
+	enz1.overhang = "G";
+	enz2.rec_seq = "GATC"; //MboI
+	enz2.overhang = "";
+	
+	//read in the fasta file
+	genome_file.open(genome_name);
+	FileTest(genome_file, genome_name);
+	count = 0;
+	while (!genome_file.eof())
+	{
+		if (!genome_file.eof())
+		{
+			universal_getline(genome_file, line);
+			if (line.substr(0, 1) == ">")
+			{
+				//it's the name
+				count++;
+				genome.push_back(fasta_record());
+				genome.back().seq_id = line.substr(1, line.size());
+			}
+			else
+			{//it's the sequence
+				if (line.substr(0, 1) != "\n")
+					genome.back().sequence.append(line);
+			}
+		}
+	}
+	genome_file.close();
+	cout << "\nSuccessfully read in " << genome.size() << " fasta records.\n";
+
+	//search through each chrom/scaffold to find restriction sites
+	cout << "\nProceeding with in silico digestion, with restriction sites " << enz1.rec_seq << " and " << enz2.rec_seq << '\n';
+	digest_file.open(digest_name);
+	for (i = 0; i < genome.size(); i++)
+	{
+		start = 0;
+		count = 0;
+		while (start != genome[i].sequence.length())
+		{
+			int first_enz1 = genome[i].sequence.find(enz1.rec_seq, start);
+			int first_enz2 = genome[i].sequence.find(enz2.rec_seq, start);
+			if (first_enz1 < first_enz2)
+			{
+				end = first_enz1;
+				overhang = enz1.overhang;
+			}
+			else
+			{
+				end = first_enz2;
+				overhang = enz2.overhang;
+			}
+			digest.push_back(fasta_record());
+			digest.back().sequence = genome[i].sequence.substr(start, end) + overhang;
+			stringstream new_name;
+			new_name << genome[i].seq_id << "_frag" << count;
+			digest.back().seq_id = new_name.str();
+			digest_file << "\n>" << digest.back().seq_id << '\n' << digest.back().sequence;
+			count++;
+			start = start + 1;
+		}
+		cout << "\nFound " << count << " fragments on " << genome[i].seq_id;
+	}
+	digest_file.close();
+	
+	cout << "Done! Input integer to quit.\n";
+	cin >> i;
+	return 0;
+}
--- scripts/compare_ddRADsdRAD.R
+++ scripts/compare_ddRADsdRAD.R
@@ -1,6 +1,125 @@
+#Author: Sarah P. Flanagan
+#Last updated: 13 September 2016
+#Date Started: 24 February 2016
+#Purpose: compare RAD libraries prepared by two different methods
 
+rm(list=ls())
 library(ggplot2)
+setwd("~/Projects/SCA/results")
 
+parse.vcf<-function(filename){
+  vcf<-read.delim(filename,comment.char="#",sep='\t',header=F,stringsAsFactors = F)
+  header.start<-grep("#CHROM",scan(filename,what="character"))
+  header<-scan(filename,what="character")[header.start:(header.start+ncol(vcf)-1)]
+  colnames(vcf)<-header
+  return(vcf)
+}
+
+vcf.cov.loc<-function(vcf.row,subset){
+  cov<-unlist(lapply(vcf.row[subset],function(x){ 
+    c<-strsplit(as.character(x),split=":")[[1]][3]
+    return(c)
+  }))
+  miss<-length(cov[cov==".,."])
+  pres<-length(cov[cov!=".,."])
+  ref<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+    function(x){
+      strsplit(as.character(x),",")[[1]][1] 
+    }))))/pres
+  alt<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+    function(x){
+      strsplit(as.character(x),",")[[1]][2] 
+    }))))/pres
+  tot<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+    function(x){
+      as.numeric(strsplit(as.character(x),",")[[1]][1]) + 
+      as.numeric(strsplit(as.character(x),",")[[1]][2])
+    }))))/pres
+  het<-unlist(lapply(vcf.row[subset],function(x){ 
+    strsplit(as.character(x),split=":")[[1]][1]
+  }))
+  het<-length(het[het=="0/1" | het=="1/0"])
+  return(data.frame(Locus=vcf.row["ID"],NumMissing=miss, NumPresent=pres,AvgCovRef=ref,
+    AvgCovAlt=alt, AvgCovTotal=tot, NumHet=het,stringsAsFactors = F))
+}
+
+vcf.cov.ind<-function(vcf.col){
+  cov<-unlist(lapply(vcf.col,function(x){ 
+    c<-strsplit(as.character(x),split=":")[[1]][3]
+    return(c)
+  }))
+  miss<-length(cov[cov==".,."])
+  pres<-length(cov[cov!=".,."])
+  ref<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+                                    function(x){
+                                      strsplit(as.character(x),",")[[1]][1] 
+                                    }))))/pres
+  alt<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+                                    function(x){
+                                      strsplit(as.character(x),",")[[1]][2] 
+                                    }))))/pres
+  tot<-sum(as.numeric(unlist(lapply(cov[cov!=".,."],
+                                    function(x){
+                                      as.numeric(strsplit(as.character(x),",")[[1]][1]) + 
+                                        as.numeric(strsplit(as.character(x),",")[[1]][2])
+                                    }))))/pres
+  het<-unlist(lapply(vcf.col,function(x){ 
+    strsplit(as.character(x),split=":")[[1]][1]
+  }))
+  het<-length(het[het=="0/1" | het=="1/0"])
+  return(list(NumMissing=miss,NumPresent=pres,AvgCovRef=ref,AvgCovAlt=alt,AvgCovTot=tot,PropHet=het/pres, NumReads=tot*pres))
+}
+  
+######FILES#####
+drad<-parse.vcf("drad.vcf")
+orad<-parse.vcf("orad.vcf")
+both<-parse.vcf("both.vcf")
+
+#########ANALYSIS##########
+#coverage per locus
+o.ind<-grep("orad",colnames(both),value=T)
+d.ind<-grep("sample",colnames(both),value=T)
+
+o.cov<-do.call("rbind",apply(both,1,vcf.cov.loc,subset=o.ind))
+d.cov<-do.call("rbind",apply(both,1,vcf.cov.loc,subset=d.ind))
+loc.cov<-merge(o.cov,d.cov,by="Locus")
+loc.cov$oRAD.AvgCovRatio<-loc.cov$AvgCovRef.x/loc.cov$AvgCovAlt.x
+loc.cov$dRAD.AvgCovRatio<-loc.cov$AvgCovRef.y/loc.cov$AvgCovAlt.y
+#because some of them have no alt
+loc.cov$oRAD.AvgCovRatio[loc.cov$oRAD.AvgCovRatio=="Inf"]<-loc.cov[loc.cov$oRAD.AvgCovRatio=="Inf","AvgCovRef.x"]
+loc.cov$dRAD.AvgCovRatio[loc.cov$dRAD.AvgCovRatio=="Inf"]<-loc.cov[loc.cov$dRAD.AvgCovRatio=="Inf","AvgCovRef.y"]
+loc.cov$oRAD.PropMiss<-loc.cov$NumMissing.x/(loc.cov$NumMissing.x+loc.cov$NumPresent.x)
+loc.cov$dRAD.PropMiss<-loc.cov$NumMissing.y/(loc.cov$NumMissing.y+loc.cov$NumPresent.y)
+loc.cov$oRAD.PropHet<-loc.cov$NumHet.x/loc.cov$NumPresent.x
+loc.cov$dRAD.PropHet<-loc.cov$NumHet.y/loc.cov$NumPresent.y
+t.test(loc.cov$oRAD.AvgCovRatio,loc.cov$dRAD.AvgCovRatio,paired=T,alternative="less")
+#private alleles
+dim(loc.cov[(loc.cov$AvgCovRef.x == 0 & loc.cov$AvgCovAlt.y == 0) | (loc.cov$AvgCovAlt.x == 0 & loc.cov$AvgCovRef.y == 0),])#2042
+dim(loc.cov[loc.cov$NumHet.x==0,])#72307
+dim(loc.cov[loc.cov$NumHet.y==0,])#14436
+#Coverage by individual--assembled in one
+o.icov<-as.data.frame(do.call("rbind",apply(both[,o.ind],2,vcf.cov.ind)))
+o.icov$Method<-rep("oRAD",nrow(o.icov))
+d.icov<-as.data.frame(do.call("rbind",apply(both[,d.ind],2,vcf.cov.ind)))
+d.icov$Method<-rep("dRAD",nrow(d.icov))
+ind.cov<-as.data.frame(rbind(o.icov,d.icov))
+ind.cov$NumReads<-as.numeric(ind.cov$AvgCovTot)*as.numeric(ind.cov$NumPresent)
+ic<-as.data.frame(apply(ind.cov,2,as.numeric))
+rownames(ic)<-rownames(ind.cov)
+ic$Method<-as.factor(ind.cov$Method)
+#Coverage by individual--assembled separately
+orad.icov<-as.data.frame(do.call("rbind",apply(orad[,10:ncol(orad)],2,vcf.cov.ind)))
+oic<-apply(orad.icov,2,as.numeric)
+rownames(oic)<-rownames(orad.icov)
+drad.icov<-as.data.frame(do.call("rbind",apply(drad[,10:ncol(drad)],2,vcf.cov.ind)))
+dic<-apply(drad.icov,2,as.numeric)
+rownames(dic)<-rownames(drad.icov)
+
+#write these to file so I can easily pick back up later
+write.table(oic,"orad_coverage.csv",quote=T,row.names=T,col.names=T,sep='\t')
+write.table(dic,"drad_coverage.csv",quote=T,row.names=T,col.names=T,sep='\t')
+write.table(ic,"both_coverage.csv",quote=T,row.names=T,col.name=T,sep='\t')
+############################ORIGINAL INVESTIGATION###################################
 setwd("E:/ubuntushare/SCA/results/biallelic/both_datasets/")
 summary<-read.delim("gwsca_summary_datasets.txt")
 
