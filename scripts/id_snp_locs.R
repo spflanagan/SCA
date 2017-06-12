3d3abd87372113fcbf5ef3b2358a6cd215edd7e3
Sarah <spflanagan.phd@gmail.com>
2017-06-12 16:51:58 -0400

    outputting outlier info

--- scripts/id_snp_locs.R
+++ scripts/id_snp_locs.R
@@ -4,17 +4,18 @@ gff<-read.delim("annotated_scovelli_scaffolds_March2016.gff",stringsAsFactors = 
 gagp<-read.delim("SSC_genome.agp",stringsAsFactors = F,skip=3,header=F)
 sagp<-read.delim("SSC_scaffolds.agp",stringsAsFactors = F,skip=3,header=F)
 agp<-rbind(gagp,sagp)
-poly.loc<-drad[,c("#CHROM","POS")]#use keep.vcf
+drad<-read.table("~/Projects/SCA/results/biallelic/keep.vcf",header=F)
+poly.loc<-drad[,1:2]#use keep.vcf
 
 #match polymorphic loci to genome
 poly.agp<-function(poly, agp,gff){
   #poly is a row of a df with chrom, pos as rows 1,2
   #agp is an agp file.
   chrom<-as.factor(unlist(poly[1]))
+  print(unlist(poly[2]))
   region<-agp[agp[,1] %in% chrom & as.numeric(agp[,2])<=as.numeric(poly[2]) & as.numeric(agp[,3]) >=as.numeric(poly[2]),]
-  reg<-data.frame(stringsAsFactors=F,
-                  cbind(Chrom=region[,1],Pos=poly[2],Chrom.start=as.numeric(region[,2]),Chrom.end=as.numeric(region[,3]),
-                        Component=region[,6],comp.start=as.numeric(region[,7]),comp.end=as.numeric(region[,8])))
+  reg<-data.frame(Chrom=region[,1],Pos=unlist(poly[2]),Chrom.start=as.numeric(region[,2]),Chrom.end=as.numeric(region[,3]),
+                        Component=region[,6],comp.start=as.numeric(region[,7]),comp.end=as.numeric(region[,8]),stringsAsFactors=F)
   adj.pos<-(as.numeric(reg$Pos)-as.numeric(reg$Chrom.start))+as.numeric(reg$comp.start)-1
   if(nrow(gff[gff$V1 %in% reg$Component,])>0){
     chrom<-reg$Component
@@ -38,7 +39,7 @@ poly.agp<-function(poly, agp,gff){
 drad.annotate<-do.call("rbind",apply(poly.loc,1,poly.agp,agp=agp,gff=gff))
 type<-drad.annotate$type
 type[type=="CDS,contig,exon,five_prime_UTR,gene,mRNA"]<-"five_prime_UTR"
-type[type=="CDS,contig,exon,gene,mRNA"]<-"gene"
+type[type=="CDS,contig,exon,gene,mRNA"]<-"exon"
 type[type=="CDS,contig,exon,gene,mRNA,three_prime_UTR"]<-"three_prime_UTR"
 type[type=="contig,exon,five_prime_UTR,gene,mRNA"]<-"five_prime_UTR"
 type[type=="contig,exon,gene,mRNA,three_prime_UTR"]<-"three_prime_UTR"
